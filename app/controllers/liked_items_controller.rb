275b4894a08b7edf794d5f346ded6d4f41d36a5f
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-07-09 01:33:02 +0000

    fixed link routing a little for aesthetics

--- app/controllers/liked_items_controller.rb
+++ app/controllers/liked_items_controller.rb
@@ -2,6 +2,6 @@ class LikedItemsController < ApplicationController
     
     def create
         Like.create(user_id: current_user.id, liked_item_id: params[:item_id])
-        redirect_to item_url(Item.find_by(id: params[:item_id]))
+        redirect_to items_path
     end
 end
